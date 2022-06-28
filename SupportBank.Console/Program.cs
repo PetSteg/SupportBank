a35941eda9a2215e29ce64151e409d2eb99084c4
Peter Stegarus <petre-florin.stegru@softwire.com>
2022-06-28 12:43:53 +0100

    implement json parser

--- SupportBank.Console/Program.cs
+++ SupportBank.Console/Program.cs
@@ -3,6 +3,8 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using CsvHelper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
@@ -14,15 +16,14 @@ namespace SupportBank.Console
         private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
         private static Dictionary<string, Person> people = new Dictionary<string, Person>();
         private static List<Transaction> transactions = new List<Transaction>();
-        private static string filePath = "../../../DodgyTransactions2015.csv";
 
-        static void ListAll()
+        private static void ListAll()
         {
             foreach (var person in people)
                 System.Console.WriteLine(person.Key + ": " + person.Value.Balance);
         }
 
-        static void ListAccount(string name)
+        private static void ListAccount(string name)
         {
             if (!people.ContainsKey(name))
             {
@@ -37,25 +38,48 @@ namespace SupportBank.Console
             }
         }
 
-        static void Main()
+        private static void ParseCSV(string filePath)
         {
-            var config = new LoggingConfiguration();
-            var target = new FileTarget
-            {
-                FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}"
-            };
-            config.AddTarget("File Logger", target);
-            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
-            LogManager.Configuration = config;
-
-            logger.Debug("Parsing file " + filePath);
+            logger.Debug("Parsing CSV file " + filePath);
             using (var reader = new StreamReader(filePath))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 // parse records and ignore invalid transactions
                 transactions = csv.GetRecords<Transaction>().Where(x => x.Amount != 0).ToList();
             }
+        }
 
+        private static void ParseJSON(string filePath)
+        {
+            logger.Debug("Parsing JSON file " + filePath);
+            string json = File.ReadAllText(filePath);
+            JArray transactionsJSON = JArray.Parse(json);
+
+            List<JToken> tokens = transactionsJSON.Children().ToList();
+
+            foreach (var token in tokens)
+            {
+                string date = token["Date"].ToString();
+                string fromAccount = token["FromAccount"].ToString();
+                string toAccount = token["ToAccount"].ToString();
+                string narrative = token["Narrative"].ToString();
+                string amount = token["Amount"].ToString();
+
+                Transaction transaction = new Transaction(date, fromAccount, toAccount, narrative, amount);
+                transactions.Add(transaction);
+            }
+        }
+
+        private static void PrintTransactions()
+        {
+            foreach (var transaction in transactions)
+            {
+                System.Console.WriteLine(transaction.ToString());
+            }
+        }
+
+        private static void ApplyTransactions()
+        {
             foreach (var transaction in transactions)
             {
                 if (!people.ContainsKey(transaction.From))
@@ -66,7 +90,10 @@ namespace SupportBank.Console
                 people[transaction.From].Balance -= transaction.Amount;
                 people[transaction.To].Balance += transaction.Amount;
             }
+        }
 
+        private static void UserConsole()
+        {
             System.Console.WriteLine(
                 "Commands:\n\"List All\" -> outputs the names of each person and their balance\n" +
                 "\"List [Name]\" -> prints a list of every transaction for the account with this name");
@@ -83,6 +110,26 @@ namespace SupportBank.Console
                 }
                 else System.Console.WriteLine("Wrong command");
             }
+        }
+
+        public static void Main()
+        {
+            var config = new LoggingConfiguration();
+            var target = new FileTarget
+            {
+                FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}"
+            };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
+
+            // ParseCSV("../../../DodgyTransactions2015.csv");
+            ParseJSON("../../../Transactions2013.json");
+
+            PrintTransactions();
+            // ApplyTransactions();
+
+            // UserConsole();
         }
     }
 }
